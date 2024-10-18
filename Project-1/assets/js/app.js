8ed140b6e87ec17595ff5ffe7c5d94bcfb4c187d
SajadAhmadi <sajad1234ahmadi@gmail.com>
2024-10-18 21:54:44 +0330

    Add files via upload

--- Project-1/assets/js/app.js
+++ Project-1/assets/js/app.js
@@ -8,3 +8,38 @@ navBtn.addEventListener("click", () => {
 });
 
 
+// ********** dropdown toggle ************
+// select button and links
+const dropBtn = document.getElementById("sd-container");
+const items = document.getElementById("sd-container-2");
+// add event listener
+dropBtn.addEventListener("click", () => {
+    items.classList.toggle("services-dropdown-show");
+});
+
+// ********** dropdown 2 toggle ************
+// select button and links
+const dropBtn2 = document.getElementById("dropdown-2");
+const items2 = document.getElementById("services-dropdown-2");
+// add event listener
+dropBtn2.addEventListener("click", () => {
+    items2.classList.toggle("services-dropdown-2-show");
+});
+
+// ********** dropdown 2.2 toggle ************
+// select button and links
+const dropBtn22 = document.getElementById("dropdown-22");
+const items22 = document.getElementById("services-dropdown-22");
+// add event listener
+dropBtn22.addEventListener("click", () => {
+    items22.classList.toggle("services-dropdown-2-show");
+});
+
+// ********** dropdown sign up toggle ************
+// select button and links
+const dropBtn3 = document.getElementById("sign-icon-nav");
+const items3 = document.getElementById("index-signup-dropdown-m");
+// add event listener
+dropBtn3.addEventListener("click", () => {
+    items3.classList.toggle("index-signup-dropdown-m-show");
+});
